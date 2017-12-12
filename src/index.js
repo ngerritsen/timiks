c0c26f7f7f572e9380bae5e1d5478f9b2d90ea26
Niels Gerritsen <n.c.gerritsen@gmail.com>
2017-12-12 20:11:55 +0100

    React app setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootEl = document.querySelector('#root');
+
+ReactDOM.render(<div>hello world</div>, rootEl);
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
