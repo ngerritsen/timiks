9a5ee8ccb3599e1c494f88ca17c28851e391ece2
Niels Gerritsen <n.c.gerritsen@gmail.com>
2017-12-14 10:52:05 +0100

    Cache bust bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 const config = {
@@ -15,14 +16,18 @@ const config = {
         loader: 'babel-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      hash: true
+    })
+  ]
 }
 
 if (process.env.NODE_ENV === 'production') {
   delete config.devtool;
-  config.plugins = [
-    new webpack.optimize.UglifyJsPlugin()
-  ];
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
 } else {
   config.devtool = 'inline-source-map';
   config.output.publicPath = '/';
