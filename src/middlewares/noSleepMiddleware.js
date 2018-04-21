4fbf9ef71594a941624f327ff8f28df50b03f3f1
Niels Gerritsen <n.c.gerritsen@gmail.com>
2018-04-21 09:53:43 +0200

    Use external nosleep

--- src/middlewares/noSleepMiddleware.js
+++ src/middlewares/noSleepMiddleware.js
@@ -1,4 +1,5 @@
-import NoSleep from 'nosleep.js/dist/NoSleep.js';
+/* global NoSleep */
+//import NoSleep from 'nosleep.js/dist/NoSleep.js';
 
 import { START_TIMER, STOP_TIMER } from '../constants/actionTypes';
 
