00bb8b124476acc7815b37205c6725fbc4dab75b
Niels Gerritsen <n.c.gerritsen@gmail.com>
2018-04-27 08:55:39 +0200

    Lower timer disabled opacity

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -65,7 +65,7 @@ const TimerTime = styled.div`
   font-size: 5rem;
   z-index: ${props => props.theme.zIndices.onFullScreenMask};
   position: relative;
-  opacity: ${props => props.disabled ? 0.6 : 1};
+  opacity: ${props => props.disabled ? 0.5 : 1};
 
   @media screen and (min-width: 420px) {
     font-size: 6rem;
