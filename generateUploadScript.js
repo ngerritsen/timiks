8db944cdad2b283011370b6e7d4fb7d7aa98ffda
Niels Gerritsen <n.c.gerritsen@gmail.com>
2019-02-03 21:05:10 +0100

    Fix upload script

--- generateUploadScript.js
+++ generateUploadScript.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const files = fs.readdirSync('public');
 const script = files
-  .map(file => `curl -H 'X-Key: $UPLOAD_KEY' -H 'X-Filepath: timiks/${file}' -F 'data=@public/${file}' https://carehr.nl/http-upload/`)
+  .map(file => `curl -H 'X-Key: '$UPLOAD_KEY -H 'X-Filepath: timiks/${file}' -F 'data=@public/${file}' https://carehr.nl/http-upload/`)
   .join('\n');
 
 fs.writeFileSync('./upload.sh', script, 'utf-8');
