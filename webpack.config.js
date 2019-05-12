b31455556334a5f76e6bcd83099cb36b5ca66afb
Niels Gerritsen <n.c.gerritsen@gmail.com>
2019-05-12 21:13:45 +0200

    Upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,41 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
-const config = {
-  entry: {
-    main: ['element-closest', path.join(__dirname, 'src/index.js')],
-    scrambleWorker: path.join(__dirname, 'src/scrambleWorker.js')
-  },
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
+const createConfig = (env, argv) => {
+  const config = {
+    entry: {
+      main: ['element-closest', path.join(__dirname, 'src/index.js')],
+      scrambleWorker: path.join(__dirname, 'src/scrambleWorker.js')
+    },
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inject: false,
+        hash: true
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: false,
-      hash: true
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  ]
+  }
+
+  if (argv.mode === 'development') {
+    config.devtool = 'inline-source-map';
+    config.output.publicPath = '/';
+  }
+
+  return config;
 };
 
-if (process.env.NODE_ENV === 'production') {
-  delete config.devtool;
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
-} else {
-  config.devtool = 'inline-source-map';
-  config.output.publicPath = '/';
-}
-
-module.exports = config;
+module.exports = createConfig;
