9428a45c2ae217f5e7ec1efc52f1de1d678de352
Niels Gerritsen <n.c.gerritsen@gmail.com>
2019-07-23 20:18:40 +0200

    Extract trainer time from trainer time table

---
+++ src/components/trainer/TrainerTime.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FontAwesome from '@fortawesome/react-fontawesome';
+import faStopwatch from '@fortawesome/fontawesome-pro-solid/faStopwatch';
+import faFlask from '@fortawesome/fontawesome-pro-solid/faFlask';
+import styled from 'styled-components';
+
+import * as CustomPropTypes from '../../propTypes';
+import Time from '../shared/Time';
+import Section from '../shared/Section';
+import { getColor } from '../../helpers/theme';
+import Button from '../shared/Button';
+import ToggleContent from '../shared/ToggleContent';
+import Modal from '../shared/Modal';
+import { buildFullCaseTitle } from '../../helpers/trainer';
+import { ButtonDuo, ButtonDuoItem } from '../shared/ButtonDuo';
+
+const TrainerTime = ({ trainerTime, removeTrainerTime, trainingType, trainingCase }) => (
+  <ToggleContent
+    toggle={({ show }) => (
+      <TrainerTimeTime onClick={show}>
+        <Time time={trainerTime} />
+      </TrainerTimeTime>
+    )}
+    content={({ hide }) => (
+      <Modal title="Remove trainer time" onClose={hide}>
+        <Section margin="md">
+          <Section margin="sm">
+            <FontAwesome icon={faFlask} fixedWidth />{' '}
+            {buildFullCaseTitle(trainingCase, trainingType)}
+          </Section>
+          <Section margin="sm">
+            <FontAwesome icon={faStopwatch} fixedWidth /> <Time time={trainerTime} />
+          </Section>
+        </Section>
+        <ButtonDuo>
+          <ButtonDuoItem>
+            <Button onClick={hide} outline color="subtleFg">
+              Cancel
+            </Button>
+          </ButtonDuoItem>
+          <ButtonDuoItem>
+            <Button
+              color="red"
+              onClick={() => {
+                removeTrainerTime(trainerTime.id);
+                hide();
+              }}
+            >
+              Remove
+            </Button>
+          </ButtonDuoItem>
+        </ButtonDuo>
+      </Modal>
+    )}
+  />
+);
+
+TrainerTime.propTypes = {
+  trainingCase: CustomPropTypes.Case.isRequired,
+  trainerTime: CustomPropTypes.TrainingTime.isRequired,
+  removeTrainerTime: PropTypes.func.isRequired,
+  trainingType: PropTypes.string.isRequired
+};
+
+const TrainerTimeTime = styled.span`
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    border-bottom: 1px solid ${getColor('fg')};
+  }
+`;
+
+export default React.memo(TrainerTime);
--- src/components/trainer/TrainerTimeTable.js
+++ src/components/trainer/TrainerTimeTable.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from '@fortawesome/react-fontawesome';
 import faTrashAlt from '@fortawesome/fontawesome-pro-solid/faTrashAlt';
-import faStopwatch from '@fortawesome/fontawesome-pro-solid/faStopwatch';
-import faFlask from '@fortawesome/fontawesome-pro-solid/faFlask';
 import styled from 'styled-components';
 
 import * as CustomPropTypes from '../../propTypes';
@@ -20,6 +18,7 @@ import { buildFullCaseTitle } from '../../helpers/trainer';
 import { ButtonDuo, ButtonDuoItem } from '../shared/ButtonDuo';
 import Shortcut from '../shared/Shortcut';
 import { Toolbar, ToolbarItem } from '../shared/Toolbar';
+import TrainerTime from './TrainerTime';
 
 const TrainerTimeTable = ({ cases, clearTrainerTimes, removeTrainerTime, trainingType }) =>
   cases.length > 0 && (
@@ -62,43 +61,11 @@ const TrainerTimeTable = ({ cases, clearTrainerTimes, removeTrainerTime, trainin
                   {trainingCase.times.map((time, i) => (
                     <span key={i}>
                       {i > 0 && <span>, &nbsp;&nbsp;</span>}
-                      <ToggleContent
-                        toggle={({ show }) => (
-                          <TrainerTime onClick={show}>
-                            <Time time={time} />
-                          </TrainerTime>
-                        )}
-                        content={({ hide }) => (
-                          <Modal title="Remove trainer time" onClose={hide}>
-                            <Section margin="md">
-                              <Section margin="sm">
-                                <FontAwesome icon={faFlask} fixedWidth />{' '}
-                                {buildFullCaseTitle(trainingCase, trainingType)}
-                              </Section>
-                              <Section margin="sm">
-                                <FontAwesome icon={faStopwatch} fixedWidth /> <Time time={time} />
-                              </Section>
-                            </Section>
-                            <ButtonDuo>
-                              <ButtonDuoItem>
-                                <Button onClick={hide} outline color="subtleFg">
-                                  Cancel
-                                </Button>
-                              </ButtonDuoItem>
-                              <ButtonDuoItem>
-                                <Button
-                                  color="red"
-                                  onClick={() => {
-                                    removeTrainerTime(time.id);
-                                    hide();
-                                  }}
-                                >
-                                  Remove
-                                </Button>
-                              </ButtonDuoItem>
-                            </ButtonDuo>
-                          </Modal>
-                        )}
+                      <TrainerTime
+                        trainerTime={time}
+                        trainingType={trainingType}
+                        trainingCase={trainingCase}
+                        removeTrainerTime={removeTrainerTime}
                       />
                     </span>
                   ))}
@@ -163,15 +130,6 @@ TrainerTimeTable.propTypes = {
   trainingType: PropTypes.string.isRequired,
   removeTrainerTime: PropTypes.func.isRequired
 };
-
-const TrainerTime = styled.span`
-  cursor: pointer;
-
-  &:hover,
-  &:focus {
-    border-bottom: 1px solid ${getColor('fg')};
-  }
-`;
 
 const PreviewWrapper = styled.span`
   width: 1.6em;
