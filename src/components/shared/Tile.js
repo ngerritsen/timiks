b2ef3eaa2cf3c3ba2a58aef1dda7bf6dfa0ec5f4
Niels Gerritsen <n.c.gerritsen@gmail.com>
2019-07-25 22:26:19 +0200

    Fix tile grid Edge

--- src/components/shared/Tile.js
+++ src/components/shared/Tile.js
@@ -6,6 +6,7 @@ import { getSize, getColor } from '../../helpers/theme';
 const Tile = styled.div`
   position: relative;
   text-align: center;
+  overflow: hidden;
   border: 1px solid ${props => getColor(props.selected ? 'blue' : 'grey')(props)};
   background-color: ${props =>
     props.selected ? transparentize(0.8, getColor('blue')(props)) : 'transparent'};
