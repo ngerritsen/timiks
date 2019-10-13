f28e2f46e01dec6f7f929136a59d3c8e005a4aca
Niels Gerritsen <n.c.gerritsen@gmail.com>
2019-10-13 20:35:42 +0200

    Remove icon from select all button

--- src/components/trainer/TrainerActions.js
+++ src/components/trainer/TrainerActions.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckSquare } from '@fortawesome/pro-regular-svg-icons/faCheckSquare';
-import { faSquare } from '@fortawesome/pro-regular-svg-icons/faSquare';
-
-import Button, { ButtonIcon } from '../shared/Button';
+import Button from '../shared/Button';
 
 const TrainerActions = ({ availableCaseIds, selectedCaseIds, selectCases, deselectCases }) => {
   if (selectedCaseIds.length > 0) {
     return (
       <Button size="sm" color="subtleBg" tag onClick={() => deselectCases(selectedCaseIds)}>
-        <ButtonIcon>
-          <FontAwesomeIcon icon={faCheckSquare} />
-        </ButtonIcon>
-        Deselect all cases
+        Deselect all
       </Button>
     );
   }
 
   return (
     <Button size="sm" color="subtleBg" tag onClick={() => selectCases(availableCaseIds)}>
-      <ButtonIcon>
-        <FontAwesomeIcon icon={faSquare} />
-      </ButtonIcon>
-      Select all cases
+      Select all
     </Button>
   );
 };
