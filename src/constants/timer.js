83fd638c49ec9214ee439cb25db1f5dab899f651
Niels Gerritsen <n.c.gerritsen@gmail.com>
2019-11-27 16:23:36 +0100

    Increase timer cooldown

--- src/constants/timer.js
+++ src/constants/timer.js
@@ -2,7 +2,7 @@ export const INSPECTION_TIME = 15000;
 export const INSPECTION_TIME_PENALTY_TIME = 2000;
 export const TIMER_UPDATE_RATE = 13;
 export const PREPARATION_STAGES = 3;
-export const TIMER_COOLDOWN = 300;
+export const TIMER_COOLDOWN = 1000;
 export const INSPECTION_TIME_WARNING_OFFSET = 400;
 
 export const inspectionTimeWarnings = [
