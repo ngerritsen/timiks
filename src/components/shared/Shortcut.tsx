7ebf4fca40774182e813fbd8ec86034fe1bf1271
Niels Gerritsen <n.gerritsen@protonmail.com>
2023-05-18 13:22:51 +0200

    Fix shortcut component

--- src/components/shared/Shortcut.tsx
+++ src/components/shared/Shortcut.tsx
@@ -17,38 +17,24 @@ const Shortcut = ({ command, action }: ShortcutProps): null => {
   const stopped = useSelector(isStopped);
   const stopTime = useSelector(getStopTime);
   const key = useMemo(
-    () =>
-      (keymap.find((mapping) => mapping.commands.includes(command)) || {}).key,
-    [command, stopped, stopTime]
+    () => (keymap.find((m) => m.commands.includes(command)) || {}).key,
+    [command]
   );
 
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [command, action]);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [command, action, stopped, stopTime]);
 
   const handleKeyDown = (event: KeyboardEvent) => {
-    const { ctrlKey, metaKey, shiftKey, altKey } = event;
+    const { ctrlKey, metaKey, shiftKey, altKey, target } = event;
+    const timerStopped = stopped && Date.now() - stopTime < TIMER_COOLDOWN;
+    const elementTag = (target as HTMLElement).tagName;
+    const isInput = inputElements.includes(elementTag.toLowerCase());
+    const isKeyCombination = ctrlKey || metaKey || shiftKey || altKey;
+    const keyMatches = keycode.isEventKey(event, key);
 
-    if (Date.now() - stopTime < TIMER_COOLDOWN) {
-      return;
-    }
-
-    if (
-      inputElements.includes(
-        (event.target as HTMLElement).tagName.toLowerCase()
-      )
-    ) {
-      return;
-    }
-
-    if (!stopped || ctrlKey || metaKey || shiftKey || altKey) {
-      return;
-    }
-
-    if (keycode.isEventKey(event, key)) {
+    if (!timerStopped && !isInput && !isKeyCombination && keyMatches) {
       event.preventDefault();
       action();
     }
